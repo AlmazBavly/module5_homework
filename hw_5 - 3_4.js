41e2ce9b72d973895848795581f4b536b200fa19
AlmazBavly <112821309+AlmazBavly@users.noreply.github.com>
2023-05-13 10:26:32 +0500

    Create hw_5 - 3_4.js

---
+++ hw_5 - 3_4.js
@@ -0,0 +1,8 @@
+// Задание 3.
+let str = "Hello";
+let reversed = str.split("").reverse().join("");
+console.log(reversed);
+
+// Задание 4.
+let randomInt = Math.floor(Math.random() * 101);
+console.log(randomInt);
