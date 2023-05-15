c5cd7ada83e664d1b2b079f8075571fa2035349c
AlmazBavly <112821309+AlmazBavly@users.noreply.github.com>
2023-05-15 22:32:04 +0500

    Create hw_5 - 6.js

---
+++ hw_5 - 6.js
@@ -0,0 +1,10 @@
+// Задание 6.
+const arr = [1, 1, 1, 1];
+let allEqual = true;
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] !== arr[0]) {
+      allEqual = false;
+      break;
+    }
+}
+console.log(allEqual);
